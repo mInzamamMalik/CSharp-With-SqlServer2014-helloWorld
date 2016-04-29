be99438bb80f38de7bdb84a7ea4f9fb1491a0616
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-29 14:40:36 +0500

    Initial commit

---
+++ database hello world/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection("server=.\\inziServer;Database=inzi-database; Integrated Security=true");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM studentTable",conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read()) {
+                Console.WriteLine("Name:{0}", reader.GetString(1));
+            }
+
+            reader.Close();
+            conn.Close();
+            if (Debugger.IsAttached) {
+                Console.ReadLine();
+            }
+        }
+    }
+}
